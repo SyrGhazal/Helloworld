cc3dec87a6ab3bbd420e7d30ab3d26fcb08ba1cb
Moultazem Ghazal <mghazal@ixini.de>
2016-11-23 12:45:59 +0100

    Hello second time
    Signed-off-by: Moultazem Ghazal <mghazal@ixini.de>

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,4 +1,11 @@
 
 public class Hello {
-
+	/*
+	 * Implement the application 
+	 * @param args arguments
+	 * */
+public static void Hello(String[] args){
+	System.out.println("Hello world!");
+	
+}
 }
